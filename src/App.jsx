89e366c81af78ed670ec6c0693842f1a61ff0351
Renz Victa <victarenz472@gmail.com>
2026-02-26 07:39:26 +0200

    Edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,20 @@ export default function OTPGenerator() {
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
+    let time;
 
-  })
+    if (isActive && timeLeft > 0) {
+      timer = setInterval() => {
+        setTimerLeft((prev) => prev - 1);
+      }, 1000);
 }
+
+if (timeLeft === 0 && isActive) {
+  setIsActive(false);
+}
+
+return () => {
+  clearInterval(timer);
+};
+  }, [timeLeft, isActive]);
+}, [timeLeft, isActive]);
