246cda2c2e5942a88026fc30469c3fac53dc3866
Renz Victa <victarenz472@gmail.com>
2026-03-04 09:01:54 +0200

    Edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import './index.css'
 
-export default function ToggleApp() {
-    const [isVisible, setIsVisible] = useState(false);
+export default function FruitsSearch() {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useStae([]);
 
-    const handleToggleVisibility = () => {
-        setIsVisible(!isVisible);
+    function handleSubmit(e) {
+        e.preventDefault();
     }
 
+    useEffect(() => {
+        if (query.trim() === '') {
+            setResults([]);
+            return;
+        }
+        const timeoutId = setTimeout(async () => {
+            try {
+                const response = await fetch(``);
+                setResults(DataTransfer.map(fruit = > fruit.name));
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }, 700);
+
+    }, [query]);
+
     return (
-        <div id="toggle-container">
-            <button onClick={handleToggleVisibility} id="toggle-button">Message</button>
-            {isVisible && <p id="message">I love freeCodeCamp!</p>}
+        <div id="search-container">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="search-input">Search for fruits:</label>
+                <input
+                    id="search-input"
+                    type="search"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+            </form>
+            <div id="results">
+                {results.length > 0 ? (
+                    results.map(item => (
+                        results.map(item => (
+                            <p key={item} className="result-item>">{item}</p>
+                        ))
+                    ) : (
+                    <p>No results found</p>
+                )}
+            </div>
         </div>
     );
-};
+}
 
