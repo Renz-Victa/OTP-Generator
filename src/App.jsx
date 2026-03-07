d2e2e7827365d50fde482feefe35c64e1183634e
Renz-Victa <victarenz472@gmail.com>
2026-03-07 09:37:08 +0200

    Edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,21 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import './index.css'
 
+
+const items = [
+  "Apples",
+  "Bananas",
+  "Strawberries",
+  "Blueberries",
+  "Mangoes",
+  "Pineapple",
+  "Lettuce",
+  "Broccoli",
+  "Paper Towels",
+  "Dish Soap",
+];
+
 export default function SuperheroForm() {
-    const powerSourceOptions = [
-        'Bitten by a strange creature',
-        'Radioactive exposure',
-        'Science experiment',
-        'Alien heritage',
-        'Ancient artifact discovery',
-        'Other'
-    ];
 
-    const powersOptions = [
-        'Super Strength',
-        'Super Speed',
-        'Flight',
-        'Invisiblity',
-        'Telekinesis',
-        'Other'
-    ];
-
-    const [heroName, setHeroName] = useState('');
-    const [realName, setRealName] = useState('');
-    const [powerSource, setPowerSource] = useState('');
-    const [powers, setPowers] = useState([]);
-
-    const handlePowersChange = e => {
-        const { value, checked } = e.target;
-        setPowers(checked ? [...powers, value] : powers.filter(p => p !== value));
-    }
-
-    return (
-        <div className='form-wrap'>
-            <h2>Superhero Application Form</h2>
-            <p>Please complete all fields</p>
-            <form method='post' action=''>
-                <div className='section'>
-                    <label>
-                        Hero Name
-                        <input
-                            type='text'
-                            value={heroName}
-                            onChange={e => setHeroName(e.target.value)}
-                        />
-                    </label>
-                    <label>
-                        Real Name
-                        <input
-                            type='password'
-                            value={realName}
-                            onChange={e => setRealName(e.target.value)}
-                        />
-                    </label>
-                </div>
-                <label className='section column'>
-                    How did you get your power?
-                    <select value={powerSource} onChange={e => setPowerSource(e.target.value)}>
-                        <option value=''>
-                            Select one
-                        </option>
-                        {powerSourceOptions.map(source => (
-                            <option key={source} value={source}>
-                                {source}
-                            </option>
-                        ))}
-                    </select>
-                </label>
-                <label className='section column'>
-                    List your powers (select all that apply);
-
-                    {powersOptions.map(power => (
-                        <label key={power}>
-                            <input
-                                type='checkbox'
-                                value={power}
-                                checked={powers.includes(power)}
-                                onChange={handlePowersChange}
-                            />
-                            <span>{power}</span>
-                        </label>
-                    ))}
-                </label>
-                <button
-                    className='submit-btn'
-                    type='submit'
-                >
-                    Join the League
-                </button>
-            </form>
-        </div >
-    )
-};
+}
 
